e6e4ad147aecc260a4238a274791b9e54848dbf5
Eltin07 <neil.tuininga@gmail.com>
2024-06-24 11:22:16 -0600

    add: category bloc provider

--- task_manager/lib/main.dart
+++ task_manager/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/presentation/bloc/all_tasks/tasks_bloc.dart';
+import 'package:task_manager/presentation/bloc/task_categories/task_categories_bloc.dart';
 import 'package:task_manager/presentation/pages/home_nav.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 
 import 'injections.dart';
 
@@ -16,8 +16,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<TasksBloc>(
-      create: (context) => sl()..add(const OnGettingTasksEvent(withLoading: true)),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TasksBloc>(
+          create: (context) => sl()..add(const OnGettingTasksEvent(withLoading: true)),
+        ),
+        BlocProvider<TaskCategoriesBloc>( // Add TaskCategoryBloc provider
+          create: (context) => sl<TaskCategoriesBloc>()..add(const OnGettingTaskCategories(withLoading: true)),
+        ),
+      ],
       child: MaterialApp(
         theme: ThemeData.light(),
         darkTheme: ThemeData.dark(),
--- task_manager/lib/presentation/widgets/Dialogs/categories_dialog.dart
+++ task_manager/lib/presentation/widgets/Dialogs/categories_dialog.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_manager/domain/models/task_category.dart';
+import 'package:task_manager/presentation/bloc/task_categories/task_categories_bloc.dart';
 
 Future<void> showCategoriesDialog(BuildContext context) async {
   return showDialog(
       context: context,
       builder: (context) {
-        return const AlertDialog(
-          title: Text("Categories"),
+        return AlertDialog(
+          title: const Text("Categories"),
+          content: BlocBuilder<TaskCategoriesBloc, TaskCategoriesState>(
+              builder: ((context, state) {
+            if (state is LoadingGetTaskCategoriesState) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is SuccessGetTaskCategoriesState) {
+              return _buildCategoryList(state.allCategories);
+            } else if (state is NoTaskCategoriesState) {
+              return const Center(child: Text("No Categories"));
+            } else {
+              return const Center(child: Text("Error has occured"));
+            }
+          })),
         );
       });
+}
+
+Widget _buildCategoryList(List<TaskCategory> categories) {
+  return Expanded(
+      child: ListView.builder(
+          itemCount: categories.length,
+          itemBuilder: ((context, index) {
+            return Text(categories[index].title);
+          })));
 }
