11256b9e3cf0f2f1d293b08282dda6a10cc3779a
Eltin07 <neil.tuininga@gmail.com>
2024-08-14 07:57:21 -0600

    update: sorting by date and priority

--- task_manager/lib/presentation/bloc/all_tasks/tasks_bloc.dart
+++ task_manager/lib/presentation/bloc/all_tasks/tasks_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:task_manager/data/entities/task_entity.dart';
 import 'package:task_manager/domain/models/task.dart';
 import 'package:task_manager/domain/usecases/tasks/add_task.dart';
 import 'package:task_manager/domain/usecases/tasks/delete_all_tasks.dart';
@@ -36,6 +37,7 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
   Future<void> _refreshTasks(Emitter<TasksState> emitter) async {
     try {
       final result = await getTaskUseCase.call();
+      final taskList = sortTasksByPriorityAndDate(result);
       final uncompleteTasks = result.where((task) => !task.isDone).toList();
       final todaysTasks = result.where((task) {
         if (task.date != null) {
@@ -50,7 +52,7 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
 
       if (result.isNotEmpty) {
         emitter(SuccessGetTasksState(
-          result,
+          taskList,
           uncompleteTasks,
           uncompleteTasks,
           todaysTasks,
@@ -88,21 +90,18 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
         List<Task> filteredTasks;
 
         if (event.filter == FilterType.all) {
-          filteredTasks = currentState.allTasks;
+          filteredTasks = sortTasksByPriorityAndDate(currentState.allTasks);
         } else if (event.filter == FilterType.date) {
-          filteredTasks = List.from(currentState.uncompleteTasks)
-            ..sort((a, b) {
-              if (a.date == null && b.date == null) return 0;
-              if (a.date == null) return 1;
-              if (b.date == null) return -1;
-              return a.date!.compareTo(b.date!);
-            });
+          filteredTasks = sortTasksByDate(currentState.uncompleteTasks);
+        } else if (event.filter == FilterType.urgency) {
+          filteredTasks = currentState.uncompleteTasks.where((task) => task.urgencyLevel == TaskPriority.high).toList();
         } else if (event.filter == FilterType.completed) {
           filteredTasks =
               currentState.allTasks.where((task) => task.isDone).toList();
         } else if (event.filter == FilterType.uncomplete) {
           filteredTasks =
               currentState.allTasks.where((task) => !task.isDone).toList();
+          filteredTasks = sortTasksByPriorityAndDate(filteredTasks);
         } else if (event.filter == FilterType.nodate) {
           filteredTasks = currentState.uncompleteTasks
               .where((task) => task.date == null)
@@ -111,6 +110,7 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
           filteredTasks = currentState.uncompleteTasks
               .where((task) => task.taskCategoryId == event.categoryId)
               .toList();
+          filteredTasks = sortTasksByPriorityAndDate(filteredTasks);
         } else if (event.filter == FilterType.overdue) {
           filteredTasks = currentState.uncompleteTasks.where((task) {
             if (task.date != null) {
@@ -124,6 +124,7 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
             }
             return false;
           }).toList();
+          filteredTasks = sortTasksByDate(filteredTasks);
         } else {
           filteredTasks = [];
         }
@@ -141,41 +142,44 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
     }
   }
 
-Future<void> _onAddTask(AddTask event, Emitter<TasksState> emitter) async {
-  try {
-    final currentState = state;
+  Future<void> _onAddTask(AddTask event, Emitter<TasksState> emitter) async {
+    try {
+      final currentState = state;
 
-    if (currentState is SuccessGetTasksState || currentState is NoTasksState) {
-      emitter(LoadingGetTasksState()); // Emit loading state while adding task
+      if (currentState is SuccessGetTasksState ||
+          currentState is NoTasksState) {
+        emitter(LoadingGetTasksState()); // Emit loading state while adding task
 
-      final newTask = await addTaskUseCase.call(event.taskToAdd);
+        final newTask = await addTaskUseCase.call(event.taskToAdd);
 
-      // Prepend the new task to the current list of tasks
-      final List<Task> updatedTaskList = [newTask, ...currentState is SuccessGetTasksState ? currentState.allTasks : []];
+        // Prepend the new task to the current list of tasks
+        final List<Task> updatedTaskList = [
+          newTask,
+          ...currentState is SuccessGetTasksState ? currentState.allTasks : []
+        ];
 
-      // Emit the updated state with the new task list
-      emitter(SuccessGetTasksState(
-        updatedTaskList,
-        updatedTaskList.where((task) => !task.isDone).toList(),
-        updatedTaskList.where((task) => !task.isDone).toList(),
-        updatedTaskList.where((task) {
-          if (task.date != null) {
-            final today = DateTime.now();
-            return task.date!.year == today.year &&
-                   task.date!.month == today.month &&
-                   task.date!.day == today.day;
-          } else {
-            return false;
-          }
-        }).toList(),
-      ));
+        // Emit the updated state with the new task list
+        emitter(SuccessGetTasksState(
+          updatedTaskList,
+          updatedTaskList.where((task) => !task.isDone).toList(),
+          updatedTaskList.where((task) => !task.isDone).toList(),
+          updatedTaskList.where((task) {
+            if (task.date != null) {
+              final today = DateTime.now();
+              return task.date!.year == today.year &&
+                  task.date!.month == today.month &&
+                  task.date!.day == today.day;
+            } else {
+              return false;
+            }
+          }).toList(),
+        ));
+      }
+    } catch (e) {
+      print('Error in _onAddTask: $e');
+      emitter(ErrorState(e.toString()));
     }
-  } catch (e) {
-    print('Error in _onAddTask: $e');
-    emitter(ErrorState(e.toString()));
   }
-}
-
 
   Future<void> _onDeleteTask(
       DeleteTask event, Emitter<TasksState> emitter) async {
@@ -232,41 +236,88 @@ Future<void> _onAddTask(AddTask event, Emitter<TasksState> emitter) async {
     }
   }
 
-Future<void> _onCompleteTask(CompleteTask event, Emitter<TasksState> emitter) async {
-  try {
-    final currentState = state;
+  Future<void> _onCompleteTask(
+      CompleteTask event, Emitter<TasksState> emitter) async {
+    try {
+      final currentState = state;
 
-    if (currentState is SuccessGetTasksState) {
-      Task taskWithCompletedDate =
-          event.taskToComplete.copyWith(completedDate: DateTime.now());
-      await updateTaskUseCase.call(taskWithCompletedDate);
+      if (currentState is SuccessGetTasksState) {
+        Task taskWithCompletedDate =
+            event.taskToComplete.copyWith(completedDate: DateTime.now());
+        await updateTaskUseCase.call(taskWithCompletedDate);
 
-      // Reorder tasks after completion to keep new tasks at the top
-      final updatedTaskList = [
-        ...currentState.allTasks.where((task) => task.id != taskWithCompletedDate.id),
-        taskWithCompletedDate
-      ];
+        // Reorder tasks after completion to keep new tasks at the top
+        final updatedTaskList = [
+          ...currentState.allTasks
+              .where((task) => task.id != taskWithCompletedDate.id),
+          taskWithCompletedDate
+        ];
 
-      emitter(SuccessGetTasksState(
-        updatedTaskList,
-        updatedTaskList.where((task) => !task.isDone).toList(),
-        updatedTaskList.where((task) => !task.isDone).toList(),
-        updatedTaskList.where((task) {
-          if (task.date != null) {
-            final today = DateTime.now();
-            return task.date!.year == today.year &&
-                   task.date!.month == today.month &&
-                   task.date!.day == today.day;
-          } else {
-            return false;
-          }
-        }).toList(),
-      ));
+        emitter(SuccessGetTasksState(
+          updatedTaskList,
+          updatedTaskList.where((task) => !task.isDone).toList(),
+          updatedTaskList.where((task) => !task.isDone).toList(),
+          updatedTaskList.where((task) {
+            if (task.date != null) {
+              final today = DateTime.now();
+              return task.date!.year == today.year &&
+                  task.date!.month == today.month &&
+                  task.date!.day == today.day;
+            } else {
+              return false;
+            }
+          }).toList(),
+        ));
+      }
+    } catch (e) {
+      print('Error in _onCompleteTask: $e');
+      emitter(ErrorState(e.toString()));
     }
-  } catch (e) {
-    print('Error in _onCompleteTask: $e');
-    emitter(ErrorState(e.toString()));
   }
-}
 
+  List<Task> sortTasksByDate(List<Task> tasks) {
+    tasks.sort((a, b) {
+      if (a.date == null && b.date == null) return 0; // Both tasks have no date
+      if (a.date == null) return 1; // a has no date, so it's placed after b
+      if (b.date == null) return -1; // b has no date, so it's placed after a
+      return a.date!
+          .compareTo(b.date!); // Compare dates if both tasks have dates
+    });
+    return tasks;
+  }
+
+  List<Task> sortTasksByPriority(List<Task> tasks) {
+    tasks.sort((a, b) {
+      int priorityComparison = _priorityValue(a.urgencyLevel)
+          .compareTo(_priorityValue(b.urgencyLevel));
+      return priorityComparison;
+    });
+    return tasks;
+  }
+
+  List<Task> sortTasksByPriorityAndDate(List<Task> tasks) {
+    tasks.sort((a, b) {
+      int priorityComparison = _priorityValue(a.urgencyLevel)
+          .compareTo(_priorityValue(b.urgencyLevel));
+      if (priorityComparison != 0) {
+        return priorityComparison;
+      }
+
+      if (a.date == null && b.date == null) return 0;
+      if (a.date == null) return 1;
+      if (b.date == null) return -1;
+      return a.date!.compareTo(b.date!);
+    });
+    return tasks;
+  }
+
+  int _priorityValue(TaskPriority? priority) {
+    if (priority == null) return 2;
+    switch (priority) {
+      case TaskPriority.high:
+        return 0;
+      case TaskPriority.none:
+        return 1;
+    }
+  }
 }
--- task_manager/lib/presentation/widgets/Dialogs/categories_dialog.dart
+++ task_manager/lib/presentation/widgets/Dialogs/categories_dialog.dart
@@ -8,79 +8,92 @@ import 'package:task_manager/presentation/widgets/category_card.dart';
 
 Future<TaskCategory?> showCategoriesDialog(BuildContext context) async {
   final TaskCategory? selectedCategory = await showDialog<TaskCategory?>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          titlePadding: EdgeInsets.zero,
-          title: Column(
-            children: [
-              Align(
-                alignment: Alignment.topRight,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const CategoryManager(),
-                      ));
-                    },
-                    child: const Text(
-                      "Manage",
-                    ),
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        titlePadding: EdgeInsets.zero,
+        title: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const CategoryManager(),
+                    ));
+                  },
+                  child: const Text(
+                    "Manage",
                   ),
                 ),
-              ), 
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  "Categories",
-                  style: TextStyle(fontSize: 20),
-                ),
-              )
-            ],
-          ),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              BlocBuilder<TaskCategoriesBloc, TaskCategoriesState>(
-                builder: (context, state) {
-                  if (state is LoadingGetTaskCategoriesState) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (state is SuccessGetTaskCategoriesState) {
-                    return _buildCategoryList(state.allCategories);
-                  } else if (state is NoTaskCategoriesState) {
-                    return const Center(child: Text("No Categories"));
-                  } else {
-                    return const Center(child: Text("Error has occurred"));
-                  }
-                },
               ),
-              ElevatedButton(
-                onPressed: () => showNewCategoryBottomSheet(context),
-                style: ElevatedButton.styleFrom(
-                  shape: const CircleBorder(),
-                ),
-                child: const Icon(Icons.add),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                "Categories",
+                style: TextStyle(fontSize: 20),
               ),
-            ],
-          ),
-        );
-      });
+            ),
+          ],
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            BlocBuilder<TaskCategoriesBloc, TaskCategoriesState>(
+              builder: (context, state) {
+                if (state is LoadingGetTaskCategoriesState) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (state is SuccessGetTaskCategoriesState) {
+                  return _buildCategoryList(state.allCategories);
+                } else if (state is NoTaskCategoriesState) {
+                  return const Center(child: Text("No Categories"));
+                } else {
+                  return const Center(child: Text("Error has occurred"));
+                }
+              },
+            ),
+            ElevatedButton(
+              onPressed: () => showNewCategoryBottomSheet(context),
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+              ),
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      );
+    },
+  );
 
   return selectedCategory;
 }
+
 
 Widget _buildCategoryList(List<TaskCategory> categories) {
   return SizedBox(
     height: 300,
     width: 200,
     child: ListView.builder(
-      itemCount: categories.length,
+      itemCount: categories.length + 1, // +1 for the "No Category" option
       itemBuilder: (context, index) {
-        return CategoryCard(category: categories[index]);
+        if (index < categories.length) {
+          return CategoryCard(category: categories[index]);
+        } else {
+          // This is the last item, so show the "No Category" option
+          return ListTile(
+            title: const Text("No Category"),
+            onTap: () {
+              Navigator.of(context).pop(null); // null indicates "No Category"
+            },
+          );
+        }
       },
     ),
   );
 }
+
