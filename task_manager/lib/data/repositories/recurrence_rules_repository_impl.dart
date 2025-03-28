3c83cfacfa1c2a512149457fc25f4e6b05e04e78
Eltin07 <neil.tuininga@gmail.com>
2025-03-28 09:07:53 -0600

    update: fill out recurring instance repo

--- task_manager/lib/data/repositories/recurrence_rules_repository_impl.dart
+++ task_manager/lib/data/repositories/recurrence_rules_repository_impl.dart
@@ -1,7 +1,6 @@
-
-
+import 'package:task_manager/domain/models/recurring_instance.dart';
 import 'package:task_manager/domain/repositories/recurrence_rules_repository.dart';
 
-class RecurrenceRulesRepositoryImpl implements RecurrenceRulesRepository{
-  
+class RecurrenceRulesRepositoryImpl implements RecurrenceRulesRepository {
+
 }
--- task_manager/lib/domain/models/recurring_instance.dart
+++ task_manager/lib/domain/models/recurring_instance.dart
@@ -1,5 +1,63 @@
-
+import 'package:flutter/material.dart';
+import 'package:task_manager/data/entities/recurring_instance_entity.dart';
 
 class RecurringInstance {
-  
+  final int? id;
+  final int? taskId;
+  final DateTime? occurrenceDate;
+  final TimeOfDay? occurrenceTime;
+  final bool isDone;
+  final DateTime? completedAt;
+
+  RecurringInstance({
+    this.id,
+    this.taskId,
+    this.occurrenceDate,
+    this.occurrenceTime,
+    this.isDone = false,
+    this.completedAt,
+  });
+
+  /// Convert from RecurringInstanceEntity to RecurringInstance
+  factory RecurringInstance.fromEntity(RecurringInstanceEntity entity) {
+    return RecurringInstance(
+      id: entity.instanceId,
+      taskId: entity.taskId,
+      occurrenceDate: entity.occurrenceDate,
+      occurrenceTime: entity.occurrenceTime,
+      isDone: entity.isDone == 1,
+      completedAt: entity.completedAt,
+    );
+  }
+
+  /// Convert to RecurringInstanceEntity
+  RecurringInstanceEntity toEntity() {
+    return RecurringInstanceEntity(
+      instanceId: id,
+      taskId: taskId,
+      occurrenceDate: occurrenceDate,
+      occurrenceTime: occurrenceTime,
+      isDone: isDone ? 1 : 0,
+      completedAt: completedAt,
+    );
+  }
+
+  /// Create a new instance with modified fields
+  RecurringInstance copyWith({
+    int? id,
+    int? taskId,
+    DateTime? occurrenceDate,
+    TimeOfDay? occurrenceTime,
+    bool? isDone,
+    DateTime? completedAt,
+  }) {
+    return RecurringInstance(
+      id: id ?? this.id,
+      taskId: taskId ?? this.taskId,
+      occurrenceDate: occurrenceDate ?? this.occurrenceDate,
+      occurrenceTime: occurrenceTime ?? this.occurrenceTime,
+      isDone: isDone ?? this.isDone,
+      completedAt: completedAt ?? this.completedAt,
+    );
+  }
 }
