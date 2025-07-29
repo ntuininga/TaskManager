39c5a26869d82179348635590ee8d08d19589b36
Neil Tuininga <neil.tuininga@gmail.com>
2025-07-29 13:20:10 -0600

    Checkbox options (#8)
    * update: settings bloc with date updates
    
    * date format
    
    * update: add checkbox selection options

--- task_manager/lib/presentation/bloc/settings_bloc/settings_state.dart
+++ task_manager/lib/presentation/bloc/settings_bloc/settings_state.dart
@@ -2,13 +2,14 @@ part of 'settings_bloc.dart';
 
 class SettingsState {
   final String dateFormat;
+  final bool isCircleCheckbox;
 
-  SettingsState({required this.dateFormat});
+  SettingsState({required this.dateFormat, required this.isCircleCheckbox});
 
-  SettingsState copyWith({String? dateFormat}) {
+  SettingsState copyWith({String? dateFormat, bool? isCircleCheckbox}) {
     return SettingsState(
       dateFormat: dateFormat ?? this.dateFormat,
+      isCircleCheckbox: isCircleCheckbox ?? this.isCircleCheckbox,
     );
   }
 }
-
